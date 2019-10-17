d1011763b1d6d7c11e6358d778c06a4b24a067c9
neotamoe <neota.moe@gmail.com>
2019-10-17 10:51:39 -0500

    event.json for local invoke; create and read working

--- articles/create.js
+++ articles/create.js
@@ -1,18 +1,38 @@
 'use strict';
 
+const aws = require('aws-sdk');
+const dynamo = new aws.DynamoDB.DocumentClient();
+const uuid = require('uuid');
+
 module.exports.handler = (event, context, callback) => {
   const data = JSON.parse(event.body)
-  if(data.text || typeof data.text !== 'string') {
+  // console.log('data.text', data.text)
+  if(data.text && typeof data.text !== 'string') {
     console.error('Validation Failed');
     callback(new Error('Body did not contain a text property'));
     return;
   }
-  console.log(data.text);
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Created article.'
-    }),
-  };
-  callback(null, response);
+
+  const params = {
+    TableName: 'BlogTable',
+    Item: {
+      article_id: uuid.v1(),
+      text: data.text
+    }
+  }
+  const putCallback = (error, result) => {
+    if(error) {
+      console.error("unable to save article.  Error JSON:", JSON.stringify(error, null, 2))
+    } else {
+      // console.log(result);  // result here is blank because we're creating something new and put method only has option to return none or old value (in situations like an update)
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify(params.Item.article_id),
+      };
+      return callback(null, response);  
+    }
+  }
+
+  dynamo.put(params, putCallback)
+ 
 };
--- articles/read.js
+++ articles/read.js
@@ -1,11 +1,35 @@
 'use strict';
 
+const aws = require('aws-sdk');
+const dynamo = new aws.DynamoDB.DocumentClient();
+
 module.exports.handler = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Read article.'
-    }),
-  };
-  callback(null, response);
+  const data = JSON.parse(event.body)
+  console.log(data.article_id)
+
+  if(data.text && typeof data.text !== 'string') {
+    console.error('Validation Failed');
+    callback(new Error('Body did not contain a text property'));
+    return;
+  }
+
+  const params = {
+    TableName: 'BlogTable',
+    Key: {
+      article_id: data.article_id
+    }
+  }
+  const getCallback = (error, result) => {
+    if(error) {
+      console.error("unable to get article.  Error JSON:", JSON.stringify(error, null, 2))
+    } else {
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify(result),
+      };
+      return callback(null, response);  
+    }
+  }
+
+  dynamo.get(params, getCallback)
 };
